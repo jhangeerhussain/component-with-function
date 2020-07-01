953b2faf52b46d7c0e5c470f1a0249b44055be62
jhangeerhussain <jhangeer.zahrsss@gmail.com>
2020-07-01 05:55:37 -0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Hi, Here I created component with Function Using JSX.
+      </h1>
+    </div>
+  )
+ 
+  
+    
+ 
+}
+
+export default App;
---
+++ src/App2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App2 = () => {
+  return React.createElement("h1" , null , "Hi, Here I created component with Function Using create Element.")
+ 
+  
+    
+ 
+}
+
+export default App2;
